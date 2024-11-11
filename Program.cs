f5660e2d96db00bab17d9e67cd01fda6253709db
Abdulloh <86368526+abdullokh425@users.noreply.github.com>
2024-11-12 01:36:55 +0500

    .

--- Program.cs
+++ Program.cs
@@ -4,11 +4,668 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
+        // 11
+        // var number = Convert.ToInt32(Console.ReadLine());
+        // var firstDigit = number / 1000 % 10;
+        // var secondDigit = number / 100 % 10;
+        // var thirdDigit = number / 10 % 10;
+        // var fourthDigit = number % 10;
+        //
+        // var extraNumber = fourthDigit;
+        // fourthDigit = thirdDigit;
+        //
+        // var resNumber = firstDigit * 1000 + secondDigit * 100 + extraNumber * 10 + fourthDigit;
+        //
+        // Console.WriteLine(resNumber);
+        
+        
+        // 12
+        // var number = Convert.ToInt32(Console.ReadLine());
+        // var firstDigit = number / 100 % 10;
+        // var secondDigit = number / 10 % 10;
+        // var thirdDigit = number % 10;
+        //
+        // var extraNumber = secondDigit;
+        // secondDigit = firstDigit;
+        //
+        // var resNumber = extraNumber * 100 + secondDigit * 10 + thirdDigit;
+        //
+        // Console.WriteLine(resNumber);
+        
+        
+        // 13
+        // var number = Convert.ToInt32(Console.ReadLine());
+        // var firstDigit = number / 1000 % 10;
+        // var secondDigit = number / 100 % 10;
+        // var thirdDigit = number / 10 % 10;
+        // var fourthDigit = number % 10;
+        //
+        // var extraNumber = thirdDigit;
+        // thirdDigit = firstDigit;
+        //
+        // var resNumber = extraNumber * 1000 + secondDigit * 100 + thirdDigit * 10 + fourthDigit;
+        //
+        // Console.WriteLine(resNumber);
+        
+        // 14
+        // var gb = Convert.ToInt16(Console.ReadLine());
+        // var mb = gb * 1024;
+        //
+        // Console.WriteLine(mb);
+        
+        
+        // 15
+        // var gb = Convert.ToInt16(Console.ReadLine());
+        // var bite = (gb * 1024l * 1024l * 1024l * 8l);
+        //
+        // Console.WriteLine(bite);
+        
+        
+        // 16
+        // var gb = Convert.ToDouble(Console.ReadLine());
+        // var tb = gb / 1000;
+        //
+        // Console.WriteLine(tb);
+        
+        
+        // 17
+        // var storage = Convert.ToInt16(Console.ReadLine());
+        // var numberOfFilms = Convert.ToDouble(storage / 2);
+        //
+        // Console.WriteLine(numberOfFilms);
+        
+        
+        // 2
+        // var number = Convert.ToInt32(Console.ReadLine());
+        // // var square = Math.Pow(number, 2);
+        // var sqnumber = number * number;
+        // var powerOfSix = sqnumber * sqnumber * sqnumber;
+        //
+        // Console.WriteLine(powerOfSix);
+        
+        
+        // 3
+        // var number = Convert.ToInt32(Console.ReadLine());
+        // // var square = Math.Pow(number, 2);
+        // var square = number * number;
+        // var powerOfFour = square * square;
+        // var powerOfEight = powerOfFour * powerOfFour;
+        //
+        // Console.WriteLine(powerOfEight);
+        
+        
+        // 1
+        // var number = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (number % 2 == 0)
+        // {
+        //     Console.WriteLine("even number");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("not even number");
+        // }
+        
+        
+        // 2
+        // var number = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (number % 2 != 0)
+        // {
+        //     Console.WriteLine("odd number");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("not odd number");
+        // }
+
+        
+        // 3
+        // var number = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (number < 100 && number > 9)
+        // {
+        //     Console.WriteLine("Two digit number");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Not two digit number");
+        // }
+        
+        
+        
+        // 4
+        // var number = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (number < 10000 && number > 999)
+        // {
+        //     Console.WriteLine("Four digit number");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Not four digit number");
+        // } 
+        
+        
+        // 5
+        // var number = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (number < 10000 && number > 999 && number % 2 == 0)
+        // {
+        //     Console.WriteLine("Four digit and even number");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("None four digit and even number");
+        // }
+        
+        
+        // 6
+        // var number = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (number < 1000 && number > 99 && number % 2 != 0)
+        // {
+        //     Console.WriteLine("Three digit and odd number");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("None three digit and odd number");
+        // }
+        
+        
+        // 7
+        // var number = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (number < 10000 && number > 999 && number % 2 == 0)
+        // {
+        //     Console.WriteLine("Four digit and even number");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("None four digit and even number");
+        // }
+        
+        
+        // 8
+        // Console.Write("Type any number: ");
+        // var number = Convert.ToInt32(Console.ReadLine());
+        // Console.Write("Type any number: ");
+        // var secondNumber = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (number % 2 != 0 && secondNumber % 2 != 0)
+        // {
+        //     Console.WriteLine("Both odd numbers");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Not odd numbers");
+        // }
+        
+        
+        // 9
+        // Console.Write("Type any number: ");
+        // var number = Convert.ToInt32(Console.ReadLine());
+        // Console.Write("Type any number: ");
+        // var secondNumber = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (number % 2 == 0 && secondNumber % 2 == 0)
+        // {
+        //     Console.WriteLine("Both even numbers");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Not even numbers");
+        // }
+        
+        
+        // 10
+        // Console.Write("Type any number: ");
+        // var number = Convert.ToInt32(Console.ReadLine());
+        // Console.Write("Type any number: ");
+        // var secondNumber = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (number % 2 == 0 && secondNumber % 2 == 0 && number > 9 && secondNumber > 9 && number < 100 && secondNumber < 100 )
+        // {
+        //     Console.WriteLine("Both odd and two digit numbers");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Both not odd and two digit numbers");
+        // }
+
+        
+        // 11
+        // var number = Convert.ToInt32(Console.ReadLine());
+        //
+        // var ones = number % 10;
+        // var tens = number / 10 % 10;
+        //
+        // if (ones == tens)
+        // {
+        //     Console.WriteLine("Equal");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Not Equal");
+        // }
+        
+        
+        // 12
+        // var number = Convert.ToInt32(Console.ReadLine());
+        //
+        // var ones = number % 10;
+        // var tens = number / 10 % 10;
+        //
+        // var sum = ones + tens;
+        //
+        // if (sum >= 10)
+        // {
+        //     Console.WriteLine("Sum is greater than 10");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Less than 10");
+        // }
+        
+        
+        // 13
+        // var number = Convert.ToInt32(Console.ReadLine());
+        //
+        // var ones = number % 10;
+        // var tens = number / 10 % 10;
+        //
+        // var multiplication = ones * tens;
+        //
+        // if (multiplication >= 20)
+        // {
+        //     Console.WriteLine("Multiplication of digits is greater than 20");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Less than 20");
+        // }
+        
+        
+        // 14
+        // var number = Convert.ToInt32(Console.ReadLine());
+        //
+        // var ones = number % 10;
+        // var tens = number / 10 % 10;
+        //
+        // if (ones % 2 != 0 && tens % 2 != 0)
+        // {
+        //     Console.WriteLine("Both numbers are odd");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Both are not odd");
+        // }
+        
+        
+        // 15
+        // var number = Convert.ToInt32(Console.ReadLine());
+        //
+        // var ones = number % 10;
+        // var tens = number / 10 % 10;
+        //
+        // if (ones > 5 && tens > 5)
+        // {
+        //     Console.WriteLine("Both numbers are greater than 5");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Both are not greater than 5");
+        // }
+        
+        
+        // 16
+        // var numberA = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (numberA % 3 == 0)
+        // {
+        //     Console.WriteLine("This number can be divided by 3");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("This number can not be divided by 3");
+        // }
+        
+        
+        // 17
+        // var numberA = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (numberA % 7 == 0)
+        // {
+        //     Console.WriteLine("This number can be divided by 3");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("This number can not be divided by 3");
+        // }
+        
+        
+        // 18
+        // var numberA = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (numberA % 3 == 0 && numberA % 8 == 0)
+        // {
+        //     Console.WriteLine("This number can be divided by 3 and 8");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("This number can not be divided by 3 and 8");
+        // }
+        
+        
+        // 19
+        // var numberA = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (numberA % 7 == 0 || numberA % 9 == 0)
+        // {
+        //     Console.WriteLine("This number can be divided by 7 or 9");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("This number can not be divided by 7 or 9");
+        // }
+        
+        
+        // 20
+        // var numberA = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (numberA % 3 == 0)
+        // {
+        //     Console.WriteLine("This number can be divided by 3");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("This number can not be divided by 3");
+        // }
+        
+        
+        // 21
+        // var numberA = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (numberA % 5 != 0)
+        // {
+        //     Console.WriteLine("This number can't be divided by 5");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("This number can be divided by 5");
+        // }
+        
+        
+        // 22
+        // var numberA = Convert.ToInt32(Console.ReadLine());
+        //
+        // if (numberA % 4 == 0 && numberA % 3 != 0)
+        // {
+        //     Console.WriteLine("This number can be divided by 4,not 3 ");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("This number can not be divided by 3");
+        // }
+        
+        
+        // 23
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        //
+        // if (numberA > numberB)
+        // {
+        //     Console.WriteLine("A greater than b");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("A is not greater than B");
+        // }
+        
+        
+        // 24
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        //
+        // if (numberA == numberB)
+        // {
+        //     Console.WriteLine("Equal");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Not Equal");
+        // }
+
+        
+        // 25
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        // var numberC = Convert.ToInt16(Console.ReadLine());
+        //
+        // if (numberA > numberB && numberA > numberC)
+        // {
+        //     Console.WriteLine("A is biggest");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("A is not the biggest number");
+        // }
+        
+        
+        
+        // 26
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        // var numberC = Convert.ToInt16(Console.ReadLine());
+        //
+        // if (numberC > numberB && numberC > numberA)
+        // {
+        //     Console.WriteLine("C is the biggest");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("C is not the biggest number");
+        // }
+        
+        
+        // 27
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        // var numberC = Convert.ToInt16(Console.ReadLine());
+        //
+        // if (numberC > numberB && numberA > numberB)
+        // {
+        //     Console.WriteLine("B is the smallest");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("B is not the smallest number");
+        // }
+        
+        
+        // 28
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        //
+        // if (numberB + numberB == numberA)
+        // {
+        //     Console.WriteLine("A is two times bigger");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("A is not two times bigger");
+        // }
+        
+        
+        // 29
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        //
+        // if (numberA + numberA + numberA + numberA + numberA == numberB)
+        // {
+        //     Console.WriteLine("B is 5 times bigger");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("B is not 5 times bigger");
+        // }
+        
+        
+        // 30
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        //
+        // if (numberA + numberA + numberA + numberA == numberB)
+        // {
+        //     Console.WriteLine("B is 4 times bigger");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("B is not 4 times bigger");
+        // }
+        
+        
+        // 10
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        //
+        // if(numberA > numberB)
+        // {
+        //     Console.WriteLine(numberA);
+        // }
+        // else
+        // {
+        //     Console.WriteLine(numberB);
+        // }
+        
+        
+        // 11
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        //
+        // if(numberA > numberB)
+        // {
+        //     Console.WriteLine(numberB);
+        // }
+        // else
+        // {
+        //     Console.WriteLine(numberA);
+        // }
+        
+        
+        // 12
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        //
+        // if(numberA > 9 && numberB > 9 && numberA < 100 && numberB < 100)
+        // {
+        //     Console.WriteLine(numberA+numberB);
+        // }
+        // else
+        // {
+        //     Console.WriteLine(numberA*numberB);
+        // }
+        
+        
+        // 13
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        //
+        // if(numberA % 2 == 0 && numberB % 2 == 0)
+        // {
+        //     Console.WriteLine(numberA+numberB);
+        // }
+        // else
+        // {
+        //     Console.WriteLine(numberA*numberB);
+        // }
+        
+        
+        // 14
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        //
+        // if(numberA % 2 != 0 && numberB % 2 != 0)
+        // {
+        //     Console.WriteLine(numberA*numberB);
+        // }
+        // else
+        // {
+        //     Console.WriteLine(numberA);
+        //     Console.WriteLine(numberB);
+        // }
+        
+        
+        // 15
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        // var numberC = Convert.ToInt16(Console.ReadLine());
+        // var count = 0;
+        //
+        // if(numberA % 2 != 0)
+        // {
+        //     count++;
+        // }
+        // if(numberB % 2 != 0)
+        // {
+        //     count++;
+        // }
+        // if(numberC % 2 != 0)
+        // {
+        //     count++;
+        // }
+        //
+        // Console.WriteLine(count);
+        
+        
+        // 16
+        // var number = Convert.ToInt16(Console.ReadLine());
+        //
+        // var extraNumber = number;
+        // var count = 0;
+        //
+        // while (extraNumber > 0)
+        // {
+        //     var digits = extraNumber % 10;
+        //     if (digits % 2 == 0)
+        //     {
+        //         count++;
+        //     }
+        //     extraNumber /= 10;
+        // }
+        //
+        // Console.WriteLine(count);
+
+        
+        // 17
+        // var numberA = Convert.ToInt16(Console.ReadLine());
+        // var numberB = Convert.ToInt16(Console.ReadLine());
+        // var numberC = Convert.ToInt16(Console.ReadLine());
+        // var numberD = Convert.ToInt16(Console.ReadLine());
+        // var numberE = Convert.ToInt16(Console.ReadLine());
+        // var count = 0;
+        //
+        // if(numberA % 2 == 0)
+        // {
+        //     count++;
+        // }
+        // if(numberB % 2 == 0)
+        // {
+        //     count++;
+        // }
+        // if(numberC % 2 == 0)
+        // {
+        //     count++;
+        // }
+        // if(numberD % 2 == 0)
+        // {
+        //     count++;
+        // }
+        // if(numberE % 2 == 0)
+        // {
+        //     count++;
+        // }
+        //
+        // Console.WriteLine(count);
     }
 }
